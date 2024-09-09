9da52c29fe93e1ecab5fb9e10570fa38339d249d
Sarah E. <tisarah94@gmail.com>
2024-09-09 14:40:54 -0300

    Corrigindo Erros e excluindo arquivo PHP (modificado)

---
+++ funcoes.js
@@ -0,0 +1,12 @@
+document.getElementById('meuFormulario').addEventListener('submit', function(event) {
+    event.preventDefault(); // Impede o envio do formulário
+
+    // Captura os dados do formulário
+    var nome = document.getElementById('nome').value;
+    var email = document.getElementById('email').value;
+
+    // Exibe os dados na página, abaixo do botão "Enviar"
+    var resultadoDiv = document.getElementById('resultado');
+    resultadoDiv.innerHTML = 'Nome: ' + nome + '<br>Email: ' + email;
+    });
+

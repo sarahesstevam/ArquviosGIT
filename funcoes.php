f8d85609e3c5e9134adc064d7cbf90d1907d1693
Sarah E. <tisarah94@gmail.com>
2024-09-09 14:31:43 -0300

    Inserindo as primeiras linhas de comando

--- funcoes.php
+++ funcoes.php
@@ -1,3 +1,13 @@
 <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $nome = htmlspecialchars($_POST['nome']);
+        $email = htmlspecialchars($_POST['email']);
     
+        // Aqui você pode adicionar o código para processar os dados, como salvar em um banco de dados ou enviar um email
+        echo "Nome: " . $nome . "<br>";
+        echo "Email: " . $email . "<br>";
+    }
+    
+    // Inclui o arquivo HTML
+    include 'index.html';
 ?>
